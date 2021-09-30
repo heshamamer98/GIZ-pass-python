d39fa2d958596250f5f7fadfb0ed32356b3213f7
heshamamer98 <heshamamerkoup@gmail.com>
2021-10-01 00:41:43 +0300

    python task by hesham amer

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,30 @@
-
-
 class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        
+        output = ""
+
+        if len(s) == 1:
+            print(s)
+        else:
+            
+            for x in range(1, len(s)):
+                if s == s[::-1]:                                    #check if s == reverse of s
+                    if len(s) > len(output):
+                        output = s
+                    break
+                else:
+                    for i in range(1, len(s)):
+                        subString = s[i:]                           #delete the first left char from s
+                        if subString == subString[::-1]:            #check if subString == reverse of subString
+                            if len(subString) > len(output):
+                                output = subString
+                            break
+                
+                s = s[:-x]                                          #delete the last char from s
+
+        print(output)
+        
+
+Solution.longest_palindromic(s="htaaaacssss")                      # output = sssss
